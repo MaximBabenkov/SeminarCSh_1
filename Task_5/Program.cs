8f5476177d6e03b2f9901fdc59b8d3a2722bd0c3
MaximBabenkov <lostbass@mail.ru>
2022-05-12 13:39:55 +0300

    Task_5_Last_Number

---
+++ Task_5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int a = int.Parse(Console.ReadLine()??"0");
+if(a>99&&a<1000)
+{
+    Console.WriteLine($"Последняя цифра этого числа={a%10}");
+}
+else
+{
+    Console.WriteLine("Это число - не трехзначное!");
+}
