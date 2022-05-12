f7386a7212a1921cd5098ceeb7de1fde75946b14
MaximBabenkov <lostbass@mail.ru>
2022-05-12 13:24:10 +0300

    Task_3_Max_Of

---
+++ Task_3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите три числа");
+int a = int.Parse(Console.ReadLine()??"0");
+int b = int.Parse(Console.ReadLine()??"0");
+int c = int.Parse(Console.ReadLine()??"0");
+int max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+Console.WriteLine($"Максимальное число={max}");
