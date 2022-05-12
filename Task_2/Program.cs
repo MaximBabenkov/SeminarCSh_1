d472adcc6f4c48dfee287a416825403322b4f460
MaximBabenkov <lostbass@mail.ru>
2022-05-12 13:14:47 +0300

    Task_2_Max_Min

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите два числа");
+int a = int.Parse(Console.ReadLine()??"0");
+int b = int.Parse(Console.ReadLine()??"0");
+int max = a;
+int min = b;
+if (a>b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine($"Максимальное число={max}");
+Console.WriteLine($"Минимальное число={min}");
